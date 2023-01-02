acef68860a8e9da00ed8ad066541389241d59654
Tom Spencer <thomas.spencer@codurance.com>
2023-01-02 12:48:33 +0000

    add code

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors');
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'taming-thymeleaf-green': colors.trueGray,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
